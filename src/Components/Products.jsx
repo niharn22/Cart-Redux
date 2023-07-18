acfcca53ba52ced54661b2ac05dd713ec60f02ff
niharn22 <niharnandoskar@gmail.com>
2023-07-18 23:27:41 +0530

    minor changes

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -31,7 +31,7 @@ const Products = () => {
                 <div className="card" key={product.id}>
                     <img src={product.image} alt=''/>
                     <h4>{product.title}</h4>
-                    <h5>{product.price}</h5>
+                    <h5>$ {product.price}</h5>
                     <button className="btn" onClick={() => handleAdd(product)}>Add to Cart</button>
                 </div>
             ))
---
+++ src/store/productSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const STATUSES = Object.freeze({
+    IDLE:'idle',
+    ERROR:'error',
+    LOADING:'loading',
+})
+
+const productSlice = createSlice({
+    name:'cart',
+    initialState:{
+        data:[],
+        status:STATUSES.IDLE,
+    },
+    reducers:{
+        setProducts(state,action){
+            state.data=action.payload
+        },
+    },
+})
+
+export const { add , remove } = cartSlice.actions;
+export default cartSlice.reducer;
